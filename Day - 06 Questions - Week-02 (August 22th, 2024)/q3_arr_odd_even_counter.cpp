b6db3619d2d8d8f84a38a1a4138702a292d87f00
Pogula Pavan Kumar Murthy <144950297+pavan-kumar-murthy@users.noreply.github.com>
2024-08-25 00:29:19 +0530

    Added solution for q3_arr_odd_even_counter.cpp

---
+++ Day - 06 Questions - Week-02 (August 22th, 2024)/q3_arr_odd_even_counter.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    
+    int arr[n];
+    int* oesum = (int*)calloc(2, sizeof(int));
+    
+    cout << "Enter the array: ";
+    
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for (int i = 0; i < n; i++) {
+        oesum[(arr[i]%2 ? 0 : 1)]++;
+    }
+
+    cout << "Sum of odd numbers: " << oesum[0] << endl;
+    cout << "Sum of even numbers: " << oesum[1] << endl;
+    return 0;
+}
