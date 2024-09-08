60949f54d77bb8f902307a9584cada5935090702
Pogula Pavan Kumar Murthy <144950297+pavan-kumar-murthy@users.noreply.github.com>
2024-09-08 22:20:33 +0530

    string_length.cpp

---
+++ Day - 08 Questions - Week-03 (August 26th, 2024)/string_length.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int stringLength(const std::string &str);
+
+int main() {
+    string str;
+    getline(cin, str);
+    
+    int length = stringLength(str);
+    cout << length << endl;
+    return 0;
+}
+
+int stringLength(const string& str) {
+    int length = 0;
+    for (int i = 0; str[i] != '\0'; i++) length++;
+    return length;
+}
